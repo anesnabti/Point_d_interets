9eb127b02be16217bbc70a2a1c42d26f2e819867
anesnabti <108154377+anesnabti@users.noreply.github.com>
2022-09-28 21:24:52 +0200

    Impact du facteur k - Ajout de la doc

--- bin/main.py
+++ bin/main.py
@@ -2,47 +2,53 @@
 
 
 """ Ce Fichier est le main """
+
+########### Import libraries #############
+import warnings
 import cv2
 from points_d_interets import * 
 from skimage.io import imread
 from skimage.color import rgb2gray
 import scipy.signal as sig
 import os
+from skimage.transform import rotate
 
 
+warnings.filterwarnings("ignore", category=np.VisibleDeprecationWarning) 
+
+
+actual_path = os.getcwd()
+cible_path = '/Point_d_interets/pics/P1.jpg'
+path = actual_path + cible_path
 # read the images 
 
-P1 = imread('C:/Users/33660/Desktop/Etudes/Master_2/SEMESTRE_1/TRAITEMENT_DES_IMAGES/TP/TP1/Point_d_interets/pics/P1.jpg')
-k_values = np.arange (0.04 , 0.06 , 0.005)
+#P1 = imread('C:/Users/33660/Desktop/Etudes/Master_2/SEMESTRE_1/TRAITEMENT_DES_IMAGES/TP/TP1/Point_d_interets/pics/P1.jpg')
+P1 = imread(path)
+cv2.imshow ('P1', P1)
+cv2.waitKey(0)
 
-#cv2.imshow ('P1', P1)
-#cv2.waitKey(0)
+I_rotate = rotate(np.copy(P1), 45)
+
+PI = Points_d_interets (P1)                                                # Instanciate the class              
+Ixx, Iyy, Ixy = PI.gradient()                                              # compute gradient  
+k_values = np.array(np.arange (0.04 , 0.06 , 0.002))
+
+image_harris = PI.harris_detector('réctangle')                             # Harris detector by rectangular window
+image_harris_gauss = PI.harris_detector('Gaussiène')                        # Harris detector by gaussian window  
+harris_cv2, nbr = PI.harris_by_cv2(0.05)                                    # Harris already implemented by cv2                  
 
 
-PI = Points_d_interets (P1)
-Ixx, Iyy, Ixy = PI.gradient()
+P_rotate = Points_d_interets(I_rotate)
+image_harris_rotate = P_rotate.harris_detector('réctangle')                             # Harris detector by rectangular window
+image_harris_gauss_rotate = P_rotate.harris_detector('Gaussiène')                        # Harris detector by gaussian window  
 
-#image_harris = PI.harris_detector('réctangle')
-image_harris_gauss = PI.harris_detector('Gaussiène')
 
-#harris_cv2 = PI.harris_by_cv2(0.05)
-#tab_of_figure = PI.harris_by_cv2_k(k_values)
-
-#PI.plot_image(image_harris,'image_harris')
-
+# ----------------- Plots -----------------#
+#PI.plot_image(image_harris,'image_harris_réctangle_window')
+#PI.plot_image(image_harris_gauss, 'harris_gaussiene')
 #PI.plot_image(harris_cv2, 'harris_cv2')
-PI.plot_image(image_harris_gauss, 'harris_gaussiene')
+#PI.plot_k_impact(k_values)
+PI.compare_methods (image_harris, image_harris_gauss, harris_cv2, "harris_réctangle", "harris_gaussiènne", "harris_cv2")
 
+PI.compare_methods (image_harris, image_harris_rotate, image_harris_gauss_rotate, "image_originale", "harris_réctangle", "harris_gaussiène")
 
-#PI.plot_multiple_images(tab_of_figure, k_values)
-
-
-
-def gkern(l=3, sig=3.):
-    """\
-    creates gaussian kernel with side length `l` and a sigma of `sig`
-    """
-    ax = np.linspace(-(l - 1) / 2., (l - 1) / 2., l)
-    gauss = np.exp(-0.5 * np.square(ax) / np.square(sig))
-    kernel = np.outer(gauss, gauss)
-    return kernel / np.sum(kernel)
--- bin/points_d_interets.py
+++ bin/points_d_interets.py
@@ -16,7 +16,6 @@ import scipy.signal as sig
 from skimage.color import rgb2gray
 
 
-
 class Points_d_interets :
 
     def __init__ (self, IMAGE) : 
@@ -34,6 +33,7 @@ class Points_d_interets :
         Ixy = Ix * Iy
         return Ixx, Iyy, Ixy
 
+
     def gauss(self, l=3, sig=3.):
         """\
         creates gaussian kernel with side length `l` and a sigma of `sig`
@@ -48,14 +48,15 @@ class Points_d_interets :
         """ l'idée est de donner à cette méthode le nom de la fentre souhaitée puis elle appliquera la détéction des coins """
         
         Ixx, Iyy, Ixy = self.gradient()
-        img_result = self.img
+        #img_result = np.copy(self.img)
 
         if window == 'Gaussiène' : 
+            img_result = np.copy(self.img)
             mask = self.gauss()
             Sxx = sig.convolve2d(Ixx,mask , mode = 'same')
             Syy = sig.convolve2d(Iyy,mask, mode = 'same')
             Sxy = sig.convolve2d(Ixy,mask, mode = 'same')
-            nbr = 0
+         
             detM = Sxx * Syy - Sxy**2
             traceM = Sxx + Syy
             C = detM - 0.05 * (traceM ** 2)
@@ -63,13 +64,14 @@ class Points_d_interets :
                 for col , r in enumerate (response) : 
                     if r > 1e-5 :            # it is a coin 
                        img_result[row,col] = [255, 0,0]           # set the point of interest to red color
-                       nbr = nbr + 1
+                       
                     else  : 
                         pass                                       # Other usecases are used to detect edges ( c > 0) or homogenous region (c = 0)
 
-            return img_result, nbr
+            return img_result
             
         elif window == "réctangle" : 
+            img_result = np.copy(self.img)
             mask = np.ones((3,3), dtype="uint8")
 
             Sxx = sig.convolve2d(Ixx, mask, mode = 'same')
@@ -78,16 +80,15 @@ class Points_d_interets :
             det = (Sxx * Syy) - (Sxy**2)
             trace = Sxx + Syy
             C = det - 0.05*(trace**2)
-            nbr = 0
+           
             for row , response in enumerate (C) : 
                 for col , r in enumerate (response) : 
                     if r > 1e-3 :            # it is a coin 
                         img_result[row,col] = [255, 0,0]           # set the point of interest to red color
-                        nbr = nbr + 1
             else  : 
-                pass 
+                pass                                               # Other usecases are used to detect edges ( c > 0) or homogenous region (c = 0)  
 
-            return img_result, nbr
+            return img_result
         
         else : 
             print("Vous êtes trompé de fentres, merci de choisir Gaussiènne ou réctangle")
@@ -96,21 +97,31 @@ class Points_d_interets :
 
     def harris_by_cv2 (self,k) : 
 
-        img_result = self.img
+        img_result = np.copy(self.img)
         dst = cv2.cornerHarris(self.I,2,3,k)
+        number_corners = np.sum(dst>0.01*dst.max())                   # compute the number of corners detected by Harris
         #result is dilated for marking the corners, not important
         dst = cv2.dilate(dst,None)
         # Threshold for an optimal value, it may vary depending on the image.
         img_result [dst>0.01*dst.max()]=[0,0,255]
-        return img_result
+        return img_result, number_corners
     
-    def harris_by_cv2_k (self,tab) : 
-        tab_of_figure = [[]]
-        for k in tab : 
-            tab_of_figure.append(self.harris_by_cv2(k))
 
-        return np.array((tab_of_figure)) 
+    def harris_by_cv2_k (self,k_values) : 
+        """  k_values : an array of multiple k values
+             return a matrix of images 
+             return the number of corner detected for any k values
+
+        """
+        tab_of_figure = [[]]
+        number_corners = []
+        for k in k_values : 
+            tab_of_figure.append(self.harris_by_cv2(k)[0])
+            number_corners.append(self.harris_by_cv2(k)[1])
+
+        return np.array((tab_of_figure)) , np.array(number_corners)
     
+
     def plot_image (self, image_to_show,title) : 
         # This function is used to show  images 
         plt.figure (3)
@@ -118,16 +129,49 @@ class Points_d_interets :
         plt.imshow(image_to_show)
         plt.show()
 
-    def plot_multiple_images (self, table_of_images, tab) : 
-        
-        for i in range (1,len(table_of_images) - 1) :
-            plt.imshow(table_of_images[i])
-            plt.title(f"k = {tab[i]} ")
-            plt.show()
 
+    def plot_k_impact (self, k_values) : 
+        """ k_values : an array of k values
+            tab_of_images : a matrix of images to plot 
+        """
+
+        #for i in range (1,len(table_of_images) - 1) :
+        #    plt.imshow(table_of_images[i])
+        #    plt.title(f"k = {tab[i]} ")
+        #    plt.show()
+
+        plt.figure (2)
+        plt.plot(k_values, self.harris_by_cv2_k(k_values)[1])
+        plt.title ("Le nombre de points d'intéret détéctées pour chaque valeur de k")
+        plt.xlabel ('k')
+        plt.ylabel("nbr de points d'intéret")
+        plt.grid()
+        plt.show()
+
+
+    def compare_methods (self, image1, image2, image3, title1, title2, title3) : 
+
+        fig = plt.figure( figsize = (8,5))
+        fig.add_subplot(1,3,1)
+        plt.imshow(image1)
+        plt.axis('off')
+        plt.title(title1)
+        #plt.show()
+
+        fig.add_subplot(1,3,2)
+        plt.imshow(image2)
+        plt.axis('off')
+        plt.title(title2)
+        #plt.show()
+
+        fig.add_subplot(1,3,3)
+        plt.imshow(image3)
+        plt.axis('off')
+        plt.title(title3)
+        plt.show()
         
 
-                    
+
 
 
 
